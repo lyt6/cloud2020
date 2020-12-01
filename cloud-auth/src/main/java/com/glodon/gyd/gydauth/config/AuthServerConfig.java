ca3cf27847858c47279033d7f4df23daf5ce6b7f
liuyongtao <847066077@qq.com>
2020-12-01 18:31:19 +0800

    rename

---
+++ cloud-auth/src/main/java/com/glodon/gyd/gydauth/config/AuthServerConfig.java
@@ -0,0 +1,12 @@
+package com.glodon.gyd.gydauth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+
+}
